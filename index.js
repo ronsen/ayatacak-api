4d0fbc52a8b80afeba8d7b9eb51bb53e37ac921e
Ronsen <ronsen@startrek.or.id>
2022-10-20 13:46:59 +0700

    enable cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { randomVerse } from './lib/random-verse.js';
 import { translateVerse } from './lib/translate-verse.js';
 import { alkitab } from './lib/alkitab.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(cors());
 app.listen(port);
 
 app.get('/', async (req, res) => {
